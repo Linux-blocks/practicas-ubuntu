8ebd864fbffaf6456258c4324d301c9f97b734da
Linux-blocks <linuxcodigo@outlook.es>
2025-04-15 19:09:54 +0200

    Update setup-lab-network.sh

--- setup-lab-network.sh
+++ setup-lab-network.sh
@@ -7,16 +7,17 @@
 VirtualBox: Crear subredes net_192.168.1, net_192.168.2 con dhcp desactivado.
 
 [R]     Ubuntu 24.04 server LTS
-  enp0s3  10.2 dhcp       vbox: net_192.168.1
-  enp0s8  192.168.1.1/24  vbox: net_192.168.1
-  enp0s9  192.168.2.1/24  vbox: net_192.168.2
+  enp0s3  10.2 dhcp       vbox: NAT
+  enp0s8  192.168.1.1/24  vbox: INTERNAL NETWORK name: LAN1,  advanced, Promiscuous Mode: Allow All
+  enp0s9  192.168.2.1/24  vbox: INTERNAL NETWORK name: LAN2,  advanced, Promiscuous Mode: Allow All
 [SWEB]  Ubuntu 24.04 server LTS
-  enp0s3  192.168.1.2/24  vbox: net_192.168.1
+  enp0s3  192.168.1.2/24  vbox: INTERNAL NETWORK name: LAN1,  advanced, Promiscuous Mode: Allow All
 [PC10]  Ubuntu 24.04 desktop LTS
-  enp0s3  192.168.1.10/24 vbox: net_192.168.1
+  enp0s3  192.168.1.10/24 vbox: INTERNAL NETWORK name: LAN1,  advanced, Promiscuous Mode: Allow All
 [PC20]  Windows 11
-  Red    192.168.2.10/24  vbox: net_192.168.2
+  Red    192.168.2.10/24  vbox: INTERNAL NETWORK name: LAN2,  advanced, Promiscuous Mode: Allow All
 
+Nota: En Windows, habilitar en FW entrada: Archivos e impresoras compartidos (petición eco: ICMP4) perfiles privado y público
 R será el FW y el proveedor de Internet (router)
 COMMENT
 
