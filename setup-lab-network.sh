d7d3cd4285482eafd6d841033ec67b1233b5d145
Linux-blocks <linuxcodigo@outlook.es>
2025-04-16 19:33:18 +0200

    Update setup-lab-network.sh

--- setup-lab-network.sh
+++ setup-lab-network.sh
@@ -21,10 +21,11 @@ Nota: En Windows, habilitar en FW entrada: Archivos e impresoras compartidos (pe
 R será el FW y el proveedor de Internet (router)
 COMMENT
 
+# Documentación netplan: https://netplan.readthedocs.io/en/stable/
 # Netplan R. Crear /etc/netplan/config.yaml
 tee > /etc/netplan/config.yaml << 'EOF'
 network:
-  renderer: 
+  renderer: networkd
   version: 2
   ethernets:
     enp0s3:
@@ -34,21 +35,57 @@ network:
     enp9s0:
       addresses: [192.168.2.1/24]
 EOF
+# comprobar errores en netplan. Aplica temporalmente las modificaciones realizadas o revierte
+netplan try
+netplan status    # comprobar config y "online status:" Si estuviera offline, ejecutar sudo netplan applu y volver a comprobar
+# aplicar netplan? en mi test tras guardar el yaml ha aplicado y se ha mantenido tras reiniciar. 
+# Parece que si se edita directamente el yaml y guarda, aplica autom. Pero aquí está la opción ...
+sudo netplan apply
 # aplica una vez guardado. Pero comprobar con
 ip addr
 
-# HABILITAR IP ROUTING en R
+# ESTABLECER ENRUTADO ENTRE LAS SUBREDES LAN1 Y LAN2. En R.
 # Permite conectar entre las LAN y estas acceder a Internet, según reglas FW cadena FORWARD
 # comprobar estado de ipv4 forwarding (routing)
 sysctl net.ipv4.ip_forward
 #sudo sysctl -w net.ipv4.ip_forward=1  # no modifica /etc/sysctl.conf. Solo durante la sesión
-: < 'EDITAR_FICHERO'
+: << 'EDITAR_FICHERO'
 /etc/sysctl.conf
 Descomentar o crear net.ipv4.ip_forward=1
 guardar
 # para aplicar lo cambios de sysctl.conf y hacer el routing permanente
 sysctl -p
 EDITAR_FICHERO
+ip route    # mostrar enrutado actual
+# IMPORTANTE: En cada cliente, hay que configurar como GW la ip asignada en la NIC asociada a la LANx
+
+# COMPROBAR CONECTIVIDAD ENTRE LANs
+ping 192.168.2.1     # desde PC10 al GW de LAN2
+ping 192.168.2.10    # desde PC10 a PC20
+ping 192.168.1.1     # desde PC20 al GW de LAN1
+ping 192.168.1.10    # desde PC20 a PC10    # si no alcanza, comprobar que se permite ping entrante en Windows
+
+# CONFIGURAR ACCESO A INTERNET PARA LAS LAN
+# reset FW:
+sudo iptables -F
+sudo iptables -X
+sudo iptables -t nat -F
+sudo iptables -t nat -X
+sudo iptables -t mangle -F
+sudo iptables -t mangle -X
+sudo iptables -P INPUT ACCEPT
+sudo iptables -P FORWARD ACCEPT
+sudo iptables -P OUTPUT ACCEPT
+
+# configurar acceso a internet de las subredes
+sudo iptables -t nat -A POSTROUTING -o enp0s3 -j MASQUERADE
+sudo iptables -A FORWARD -i enp0s3 -o enp0s8 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i enp0s8 -o enp0s3 -j ACCEPT
+sudo iptables -A FORWARD -i enp0s3 -o enp0s9 -m state --state RELATED,ESTABLISHED -j ACCEPT
+sudo iptables -A FORWARD -i enp0s9 -o enp0s3 -j ACCEPT
+
+
+
 
 
 
