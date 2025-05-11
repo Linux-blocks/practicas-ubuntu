1b69c0b5a090c86eea5932c426809623fa70c14e
Linux-blocks <linuxcodigo@outlook.es>
2025-05-12 00:36:22 +0200

    Update setup-dnsserver.sh

--- setup-dnsserver.sh
+++ setup-dnsserver.sh
@@ -18,8 +18,8 @@ systemctl status bind9  # o también por 'named'
 
 # EDITAR opciones del servidor, guardando el fichero instalado por defecto
 sudo cp /etc/bind/named.conf.options /etc/bind/named.conf.options.default
-tee > /etc/bind/named.conf.options < 'EOF'
-acl LANS { 192.168.1.0/24; 192.168.2.0/24; }
+tee /etc/bind/named.conf.options < 'EOF'
+acl LANS { 192.168.1.0/24; 192.168.2.0/24; };
 
 options {
   directory "/var/cache/bind";
@@ -27,7 +27,7 @@ options {
   recursion yes;
   forwarders { 1.1.1.1; };
 # si solo se desea escuchar en un interface (NIC), p.e enp0s8 (192.168.1.1)
-#  listen-on { 192.168.1.1 };
+#  listen-on { 192.168.1.1; };
 # si solo permitir el uso del dns internamente:
 #  allow-recursion { LANS; };
 #  allow-query-cache { LANS; };
@@ -46,16 +46,44 @@ ip -brief addr show to 192.168.1.0/24  # o abrevia con 'ip a' y busca
 : << 'COMMENT'
 Se añadirá al fichero, dentro de la sección network: - ethernets: - <nic_name>:
   nameservers:
-    addresses: [ 10.0.2.1 ]       # ip del servidor dns [R]
+    addresses: [ 10.0.2.1, 8.8.4.4 ]       # ip del servidor dns [R]
     search [ midominio.example ]  # si se configuró una zona en el servidor dns
 COMMENT
 # Editando directamente el fichero, aplica al guardarlo
 # Si se edita netplan con comandos CLI:
 sudo netplan try    # aplica con cuenta atrás para revertir a config anterior, o enter para aplicar definitivamente
-sudo netplan apply  # aplicar configuración por CLI
+sudo netplan -debug apply  # aplicar configuración por CLI
 netplan status      # comprobar config y "online status:" Si estuviera offline, ejecutar sudo netplan apply y volver a comprobar
 # comprobar estado de red actual:
 ip addr
+
+: << 'COMMENT'
+Cuando se hace una consulta dns domain.ext, si se usa netplan, 
+primero pasa por el resolver systemd-resolved (con un stub listener), 
+configurado en /run/systemd/resolve/stub-resolv.conf, 
+que afecta a /etc/systemd/resolve/resolv.conf,
+que devolverá resultado cacheado 
+o lo pasará a los nameservers configurados en netplan NIC
+systemd-resolve --status nos muestra la lista de upstream DNS servers
+Comprobar también si está instalado resolvconf (systemctl status resolvconf), desactivarlo.
+O deshabilitar systemd-resolved y crear manualmente /etc/resolv.conf
+(que es sobreescrito por systemd-resolved, por lo que los cambios son temporales)
+++ Cambios via resolve.conf
+Para config permanentes, editar /etc/systemd/resolve/resolv.conf o /etc/systemd/resolved.conf
+DNS=<ip> <ip>      # primarios
+FallbackDNS=<ip>   #
+y systemctl restart systemd-resolved
+check: resolvectl status  # de cada ethernet
+
+++ cambios vía netplan (config nameservers)
+Modificar el yaml con nameservers -addresses para las NIC
+
+** Se puede desactivar el stub listener, y liberar el puerto 23 para el dns server, 
+editando /etc/system/resolve/resolv.conf DNSStubListener=no
+netplan try y posiblemente reiniciar el servidor con sudo reboot.
+COMMENT
+
+# CONFIGURAR FIREWALL para permitir in a 53 UDP
 
 # comprobar configuración de zona dns midominio.example, por nombre y por fqdn
 # desde [R] y desde otro cliente en LANS
