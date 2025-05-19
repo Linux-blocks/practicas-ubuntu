20904e65a9bb68b9e3d83026025a274847bd5857
Linux-blocks <linuxcodigo@outlook.es>
2025-05-20 00:20:55 +0200

    Update set-remote-file-transfers.sh

--- set-remote-file-transfers.sh
+++ set-remote-file-transfers.sh
@@ -28,6 +28,7 @@ REMOTE_DIR='/etc/cron.d'
 LOCAL_DIR=/home/usuario/bck
 BCK_FILE=fichero-$(date + '%Y%m%d%H%M').tar
 # -q no muestra el banner de conexión, -o auto-acepta la host-key en primera conexión al servidor ssh
+# -z comprime en gz
 ssh -q -p 15002 -o BatchMode=true -o StricthHostKeyChecking=accept-new {REMOTE_USER}@${REMOTE_IP} "tar -czf - ${REMOTE_DIR}" > "${LOCAL_DIR}/${BCK_FILE}"
 if [0 -eq $?]; then
   echo 'Copia realizada'
@@ -38,49 +39,23 @@ fi
 ############### CREAR CRON DE SCRIPT) ###############
 # formato tarea: m h month-day month weekday command
 # cron tutorial: https://www.hostinger.com/es/tutoriales/cron-job
-crontab -e      # abre el editor de crons del usuario
+crontab -e      # abre el editor de crons del usuario actual. Añadir -u <username> para el de uno en concreto
 : << 'CONTENIDO'
 5  0  *  *  *  /home/usuario/backup.sh >> /home/backups.log
 CONTENIDO
 
 # comprobar contenido para usuario actual
-crontab -l
-
+crontab -l   # [-u <username>]
 
 : << 'STEPS'
 
 Agregar tarea a nivel de usuario
 --------------------------------
-Fichero /etc/crontab
+/var/spool/cron/crontabs/<username>
 
 Agregar tarea  a nivel de sistema
 ---------------------------------
 /etc/cron.d/  /ect/cron.[hourly|daily|weekly|monthly|yearly]/
-
-
-(A) Obtener carpeta remota a local. No pide clave si el usuario local y remoto son el mismo (id y password)
-scp -rp remote_username@remote_ip:/ruta/carpeta /ruta/local
-
-Ejemplo: 
-mkdir -p /home/usuario/backups
-scp -rp -P 15002 usuario@192.168.1.1:/etc/cron.d /home/usuario/backups
-
-(B) Obtener carpeta remota que se debe comprimir en remoto, el contenido contenido se transmite y se crea un fichero comprimido en local
-Formato script, no olvida hacer ejecutable el fichero. Interactivo, pedirá contraseña del 'usuario':
-
-#!/bin/bash
-REMOTE_USER=usuario
-REMOTE_IP=192.168.1.1
-REMOTE_DIR='/etc/cron.d'
-BCK_FILE=fichero-$(date + '%Y%m%d%H%M').tar
-# -q no muestra el banner de conexión -o auto-acepta la host-key en primera conexión al servidor ssh
-ssh -q -o StricthHostKeyChecking=accept-new {REMOTE_USER}@${REMOTE_IP} "tar -zcf - ${REMOTE_DIR}" > "${LOCAL_DIR}/${BCK_FILE}"
-
-if [0 -eq $?}; then
-  echo 'Copia realizada'
-else
-  echo 'Error al copiar'
-fi
-
-
+/etc/crontab  Incluye un campo añadido min hour day month wday <username> script|command
+              Incluye runs para cada .hourly daily weekly monthly ...
 STEPS
